0b03cc77fb999bb8c38a8bb4ce6817112659d921
Dannie Taplin <dtaplin@my365.bellevue.edu>
2023-08-19 21:47:12 -0500

    week 2 deploy

---
+++ week-2/taplin_myworld.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------
+# taplin_myworld.py
+# Danielle Taplin
+# 08/19/23
+# week 2 code, welcome to my world
+#----------------------------------------------------------
+
+#Declare a variable to hold a last name
+last_name = "Taplin"
+
+#Print text to the screen using the declared last name
+print("You are in " + last_name + "'s world!")
